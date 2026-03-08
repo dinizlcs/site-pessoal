21af0ea19a11f542c98e58086f69afdb35782767
Lucas Diniz <lucdinizcontato@gmail.com>
2026-03-08 01:45:26 -0300

    Página principal/Sobre
    Área sobre finalizada e mais ajustes da base

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <title>Portfólio-Lucas Diniz</title>
 
     <?php echo Base::carregarLinks(); ?>
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
     <!-- Menu: Telas menores -->
@@ -31,8 +32,23 @@
     
     <!-- Conteúdo da página -->
     <main>
-        <div id="container-conteudo">
-            <p>Conteúdo da página aqui</p>
+        <div id="container-conteudo" class="centralizar-itens-h">
+            <h2>Lucas Diniz de Oliveira</h2>
+            <?php
+                $idadeAtual = date("Y") - 2003;
+
+                echo <<<HTML
+                    <p>
+                        Atualmente tenho $idadeAtual anos, possuo formação técnica em <strong>Análise e Desenvolvimento de Sistemas (ADS)</strong> pelo Senac e estou cursando <strong>Segurança da Informação</strong> pela FMU.
+                    </p>
+                    <p>
+                        Meu primeiro contato com programação ocorreu aos 15 anos, quando comecei a desenvolver soluções em planilhas do Excel utilizando Visual Basic for Applications (VBA). Por meio do VBA, desenvolvo formulários que facilitam o preenchimento de dados, além de rotinas voltadas para a organização de informações, entre outras funcionalidades. Essa continua sendo a linguagem com a qual possuo maior domínio.
+                    </p>
+                    <p>
+                        Em 2019, iniciei minha trajetória no <strong>desenvolvimento web</strong> por meio de cursos de <strong>HTML5 e CSS3</strong>. Posteriormente, aprofundei meus conhecimentos web aprendendo <strong>JavaScript e JQuery</strong>. Em 2021, direcionei meus estudos para o <strong>desenvolvimento backend</strong>, passando a utilizar <strong>PHP</strong>. Em 2024, concluí o curso técnico de ADS aprendendo principalmente <strong>Java e MySQL</strong> e, nesse período, decidi direcionar de vez minha carreira para o funcionamento interno dos sistemas, em vez de focar na parte visual. Em outras palavras, tenho maior interesse em garantir que as funcionalidades de um sistema operem corretamente, como o envio e o armazenamento das informações inseridas, do que em atuar diretamente na interface visual.
+                    </P>
+HTML;
+            ?>
         </div>
     </main>
 </body>
